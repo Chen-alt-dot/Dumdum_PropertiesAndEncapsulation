0fde15c5ff7efd0c030c2f3b95d538c05aab99e8
ACT-STUDENT <ACT-STUDENT@LAB3-PC39>
2025-08-20 10:25:34 +0800

    exercise 4

---
+++ Dumdum_PropertiesAndEncapsulation/Dumdum_PropertiesAndEncapsulation/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dumdum_PropertiesAndEncapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instantiate a shoe object
+            Shoe shoe1 = new Shoe();
+
+            //Setting Values for properties
+            shoe1.Brand = "JORDAN";
+            shoe1.Size = 10;
+
+            //Display the shoe's details
+            shoe1.DisplayShoeInfo();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Dumdum_PropertiesAndEncapsulation/Dumdum_PropertiesAndEncapsulation/Shoe.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dumdum_PropertiesAndEncapsulation
+{
+    internal class Shoe
+    {
+        // Name: Christian Benedict P. Dumdum
+        // Section: IT301
+        // EXER_04: Basics on Encapsulation
+
+        //Fields
+        private string brand;
+        private int size;
+
+
+        //Public property for Brand
+        public string Brand
+        {
+            get { return brand; }
+            set { brand = value; }
+        }
+
+        //Public property for Size
+        public int Size
+        {
+            get { return size; }
+            set { size = value; }
+        }
+
+        //Method
+        public void DisplayShoeInfo()
+        {
+            Console.WriteLine("Your Shoe Info!!!");
+            Console.WriteLine($"Brand: {Brand}");
+            Console.WriteLine($"Size: {Size}");
+        }
+
+    }
+}
